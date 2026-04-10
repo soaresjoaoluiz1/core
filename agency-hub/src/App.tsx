556e8d6ab02deed675decc72638501ab33bcfb0c
Agenciadouc <agenciadouc@gmail.com>
2026-04-10 07:41:10 -0300

    Revert "feat: task templates with parent/child subtasks"
    This reverts commit 62e5acff84ef86934358d7eb5a2e91afa2040217.

--- agency-hub/src/App.tsx
+++ agency-hub/src/App.tsx
@@ -16,7 +16,6 @@ import Categories from './pages/Categories'
 import SettingsPage from './pages/Settings'
 import Notifications from './pages/Notifications'
 import Services from './pages/Services'
-import Templates from './pages/Templates'
 import Financial from './pages/Financial'
 import Onboard from './pages/Onboard'
 import TimerCheck from './components/TimerCheck'
@@ -53,7 +52,6 @@ function AppRoutes() {
               <Route path="/departments" element={<Departments />} />
               <Route path="/categories" element={<Categories />} />
               <Route path="/services" element={<Services />} />
-              <Route path="/templates" element={<Templates />} />
               <Route path="/financial" element={<Financial />} />
               <Route path="/settings" element={<SettingsPage />} />
             </>}
